2834863793118c31cb3fa96dad5b8eba56e807ab
Conor Donnelly <donn0021@umn.edu>
2022-12-01 11:02:43 -0600

    got express routing working for CRUD on Categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,64 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+  });
+
 });
 
+
+// find one category by its `id` value
+// be sure to include its associated Products
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
+
+// create a new category
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create(req.body)
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
+// update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    { category_name: req.body.category_name },
+    {
+      where: {
+        id: req.params.id
+      },
+    }
+  )
+    .then((upCategory) => {
+      res.json(upCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
+
+// delete route for a category by its `id` value
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
