0dff880b5060056ab89d9d19021dced7901e5345
Conor Donnelly <donn0021@umn.edu>
2022-11-30 20:24:23 -0600

    set up .env file with credentials and schema.sql with create table statements

---
+++ Develop/db/schema.sql
@@ -0,0 +1,35 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
+
+USE ecommerce_db;
+
+CREATE TABLE category (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(30) NOT NULL  
+);
+
+CREATE TABLE product (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(30) NOT NULL,
+  price DECIMAL NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES category(category_id)
+);
+
+CREATE TABLE tag (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(30)
+);
+
+CREATE TABLE producttag (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  tag_id INT,
+  FOREIGN KEY (product_id) REFERENCES product(product_id),
+  FOREIGN KEY (tag_id) REFERENCES tag(tag_id) 
+);
+
